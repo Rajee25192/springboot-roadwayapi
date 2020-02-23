f800d4cabcd9495c393523c3029637839f420b29
Kumar <rajekuma@rajekuma-147aff.1499.lowes.com>
2020-02-23 23:28:51 +0530

    Initial commit to add the project

---
+++ ReadMe.md
@@ -0,0 +1,27 @@
+Road Way Service Spring Boot Application
+This application will help you in find the direct route or connected routes of two cities with all the necessary logics  required for finding the connected routes if there is no direct path is available. It is been created by using maven archetype from Spring initialzr with swagger been included for documentation
+
+Prerequisites:
+==============
+
+Check the city.txt file is under the classpath with the city pair of list data present and load the project by running spring boot appliction.
+
+
+Testing URLs
+============
+GET METHOD:
+http://localhost:8080/connected?origin=Philadelphia&destination=Boston
+
+For Swagger
+============
+
+Start the applcation and try to open the below URL in any browser.. You can use the swagger for getting the information about the API and test the API with the known data.
+
+http://localhost:8080/swagger-ui.html
+
+
+Code Owners
+For bugs/queries please reach out to us -
+
+Name :
+Email ID:
---
+++ src/test/java/com/roadmap/roadmapservice/controller/RoadmapControllerTest.java
@@ -0,0 +1,33 @@
+package com.roadmap.roadmapservice.controller;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class RoadmapControllerTest {
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void shouldReturnDefaultMessage() throws Exception {
+		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isNotFound())
+				.andExpect(content().string(containsString("")));
+	}
+	
+	@Test
+	public void shouldReturnSuccessMessage() throws Exception {
+		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isNotFound())
+				.andExpect(content().string(containsString("")));
+	}
+}
+
